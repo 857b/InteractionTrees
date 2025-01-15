5854f8dfb9a85e833b30eba959e7e124bc9d4ba4
Benjamin Bonneau <27144402+857b@users.noreply.github.com>
2025-01-15 15:01:38 +0100

    test: check universes

---
+++ tests/check-universes/CheckUniverses.v
@@ -0,0 +1,9 @@
+From ITree Require
+  ITree ITreeFacts Events.
+
+Constraint ITreeDefinition.u_itree < Datatypes.sum.u0.
+Constraint ITreeDefinition.u_itree < Datatypes.sum.u1.
+Constraint ITreeDefinition.u_itree < Datatypes.prod.u0.
+Constraint ITreeDefinition.u_itree < Datatypes.prod.u1.
+Constraint ITreeDefinition.u_itree < Datatypes.option.u0.
+Constraint ITreeDefinition.u_itree < Datatypes.list.u0.
